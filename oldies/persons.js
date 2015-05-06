22515603a32033578e333f15aa2734a4c24fb918
Jarl Holmberg Lindquist <jarllindquist@gmail.com>
2015-05-06 18:12:31 +0200

    lägg till filer toll oldier

---
+++ oldies/persons.js
@@ -0,0 +1,39 @@
+/*recept från orientalisk omelett*/
+/*utgår ifrån 1 person */
+var ägg = 0,75
+var kötfärs = 25 
+var tomatpure = 0,75 tsk 
+var Tomat = 0,25 finhackad
+
+
+0,75 Ägg
+25 gram Köttfärs
+12,5 gram Färsk Mangold blad eller Spenat blad
+¼ tsk Tomatpuré
+¼ finhackad Tomat
+Salt o peppar efter smak
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function greet(){
+	var name = prompt("Vad heter du?");
+	//var time = time();
+	alert("Hej " + name);
+	}
+function init() {
+	but = document.getElementById("numbs");
+	but.onclick=greet;
+	}
+	
+	window.onload=init;
